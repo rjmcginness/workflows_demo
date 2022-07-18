a8f7c54776a073b6069e2c21c8658fe476d0825f
Robert J. McGinness <robertjmcginness5@gmail.com>
2022-07-17 20:31:51 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,13 @@
+import sys
+
+class VersionException(Exception):
+    pass
+
 def main():
-    print('Hello, World!')
+    print('Hello, World! From Python:' + str(sys.version_info))
+    if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
+        # intentional fail for Python 3.6
+        raise VersionException('Python version 3.6.x is unsupported!')
 
 
 if __name__ == '__main__':
